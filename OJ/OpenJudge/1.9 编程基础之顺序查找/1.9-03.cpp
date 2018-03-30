e79a53e7fa92e80b604f5dc0d4b8bb986beb0e33
yhzhm <yhzhm@qq.com>
2018-03-30 20:28:27 +0800

    upload

--- OJ/OpenJudge/1.9 编程基础之顺序查找/1.9-03.cpp
+++ OJ/OpenJudge/1.9 编程基础之顺序查找/1.9-03.cpp
@@ -5,7 +5,7 @@ int main()
 	int max = 0, n1, n2, d = 0;
 	for (int i = 1; i <= 7; ++i) {
 		cin >> n1 >> n2;
-		if (max < n1 + n2) {
+		if (n1 + n2 > 8 && max < n1 + n2) {
 			max = n1 + n2;
 			d = i;
 		}
