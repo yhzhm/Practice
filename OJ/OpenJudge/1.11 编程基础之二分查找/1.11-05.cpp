6b8d5626ef90d259dc2f4452d3516af2de57b6e7
yhzhm <yhzhm@qq.com>
2018-04-04 07:21:01 +0800

    upload

--- OJ/OpenJudge/1.11 编程基础之二分查找/1.11-05.cpp
+++ OJ/OpenJudge/1.11 编程基础之二分查找/1.11-05.cpp
@@ -1,32 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,f;
+int n, f;
 double a[10010];
-const double PI=acos(-1.0);//π精度要求很高 
+const double PI = acos(-1.0); //π精度要求很高
 bool check(double mid)
 {
-	int sum=0;
-	for(int i=1;i<=n;++i){
-		sum+=a[i]/mid;
+	int sum = 0;
+	for (int i = 1; i <= n; ++i) {
+		sum += a[i] / mid;
 	}
-	return sum>=f+1;
+	return sum >= f + 1;
 }
 int main()
 {
-	cin>>n>>f;
+	cin >> n >> f;
 	int x;
-	double l=0.0,r=0.0,mid;
-	for(int i=1;i<=n;++i){
-		cin>>x;
-		a[i]=x*x*PI;
-		r=max(r,a[i]);
+	double l = 0.0, r = 0.0, mid;
+	for (int i = 1; i <= n; ++i) {
+		cin >> x;
+		a[i] = x * x * PI;
+		r = max(r, a[i]);
 	}
-	while(r-l>1e-5){
-		mid=l+(r-l)/2;
-		if(check(mid)) l=mid;
-		else r=mid;
+	while (r - l > 1e-5) {
+		mid = l + (r - l) / 2;
+		if (check(mid)) l = mid;
+		else r = mid;
 	}
-	printf("%.3f\n",l);	
+	printf("%.3f\n", l);
 	return 0;
 }
 
