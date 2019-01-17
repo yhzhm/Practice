7898914324525d18cc8bc4f6657a6b79517c8a30
yhzhm <yhzhm@qq.com>
2019-01-17 15:06:44 +0800

    sync

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
+<<<<<<< HEAD
 #include <iostream>
 #include <algorithm>
+=======
+// Created by Hz Yang on 2017.05
+// #include <bits/stdc++.h>
+#include <iostream>
+>>>>>>> 1228bb1643cb3521c30396d68938d8cf2d96e1ca
 using namespace std;
 bool isp(int n)
 {
+<<<<<<< HEAD
 	if (n < 2) return false;
 	for (int i = 2; i * i <= n; ++i) {
 		if (n % i == 0) return false;
 	}
 	return true;
+=======
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; ++i){
+        cout << i << ' ';
+    } 
+    return 0;
+>>>>>>> 1228bb1643cb3521c30396d68938d8cf2d96e1ca
 }
 int main()
 {
