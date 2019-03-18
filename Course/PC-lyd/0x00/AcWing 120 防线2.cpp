aed3f614380550849b34cf1fc0b34e6d4692315b
yhzhm <yhzhm@qq.com>
2019-03-18 10:37:14 +0800

    AcWing

--- Course/PC-lyd/0x00/AcWing 120 防线2.cpp
+++ Course/PC-lyd/0x00/AcWing 120 防线2.cpp
@@ -5,10 +5,10 @@ const int M = 2e5 + 10;
 int n;
 struct Seq {int s, e, d;} seqs[M];
 
-int get_sum(int x)
+int get_sum(int x)//当前位置的前缀和
 {
 	int res = 0;
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)//遍历所有点
 		if (seqs[i].s <= x) res += (min(seqs[i].e, x) - seqs[i].s) / seqs[i].d + 1;
 	return res;
 }
@@ -19,20 +19,20 @@ int main()
 	while (T--) {
 		int l = 0, r = 0;
 		scanf("%d", &n);
-		for (int i = 0; i < n; ++i) {
+		for (int i = 0; i < n; ++i) {//读入每段的s，e，q,将最大的e作为r
 			int s, e, q;
 			scanf("%d%d%d", &s, &e, &q);
 			seqs[i] = {s, e, q};
 			r = max(r, e);
 		}
 
-		while (l < r) {
+		while (l < r) {//因为奇数的点有且仅有1个，所以前缀和的奇偶性具有单调性，以此二分求奇数点
 			int mid = l + r >> 1;
 			if (get_sum(mid) & 1) r = mid;
 			else l = mid + 1;
 		}
 
-		auto sum = get_sum(r) - get_sum(r - 1);
+		auto sum = get_sum(r) - get_sum(r - 1);//求二分到的点的防具数量
 		if (sum % 2) printf("%d %d\n", r, sum);
 		else puts("There's no weakness.");
 	}
--- Course/PC-lyd/0x00/AcWing 121 赶牛入圈3.cpp
+++ Course/PC-lyd/0x00/AcWing 121 赶牛入圈3.cpp
@@ -3,25 +3,26 @@
 using namespace std;
 const int M = 1010;
 typedef pair<int, int> PII;
-int n, C, sum[M][M];
-PII points[M];
-vector<int> numbers;
+int n, C, sum[M][M];//sum[][]用于求离散化后的二维前缀和
+PII points[M];//用于保存点坐标
+vector<int> numbers;//用于离散化点坐标
 
-int get(int x)
+int get(int x)//二分求当前点在numbers中的下标
 {
-	int l = 0, r = numbers.size() - 1;
+	/*int l = 0, r = numbers.size() - 1;
 	while (l < r) {
 		int mid = l + r >> 1;
 		if (numbers[mid] >= x) r = mid;
 		else l = mid + 1;
 	}
-	return r;
+	return r;*/
+	return lower_bound(numbers.begin(), numbers.end(), x) - numbers.begin();
 }
 
 bool check(int len)
 {
-	for (int x1 = 0, x2 = 1; x2 < numbers.size(); x2++) {
-		while (numbers[x2] - numbers[x1 + 1] + 1 > len) x1++;
+	for (int x1 = 0, x2 = 1; x2 < numbers.size(); x2++) {//将x2，x1的草都包含的话，长度是numbers[x2]-numbers[x1]+1
+		while (numbers[x2] - numbers[x1 + 1] + 1 > len) x1++;//如果下一个位置不能包含进来的话（不在len范围内），则需要考x1++
 		for (int y1 = 0, y2 = 1; y2 < numbers.size(); y2++) {
 			while (numbers[y2] - numbers[y1 + 1] + 1 > len) y1++;
 			if (sum[x2][y2] - sum[x1][y2] - sum[x2][y1] + sum[x1][y1] >= C)
@@ -34,28 +35,28 @@ bool check(int len)
 int main()
 {
 	cin >> C >> n;
-	numbers.push_back(0);
-	for (int i = 0; i < n; ++i) {
+	numbers.push_back(0);//先放1个零，方便后面做前缀和（sum[x][y],x,y的值（坐标在numbers中的下标）最小从1开始）
+	for (int i = 0; i < n; ++i) {//读入点坐标
 		int x, y;
 		cin >> x >> y;
 		points[i] = {x, y};
-		numbers.push_back(x);
+		numbers.push_back(x);//将横坐标和纵坐标都存储到vector<int> numbers中
 		numbers.push_back(y);
 	}
 
-	sort(numbers.begin(), numbers.end());
-	numbers.erase(unique(numbers.begin(), numbers.end()), numbers.end());
+	sort(numbers.begin(), numbers.end());//排序
+	numbers.erase(unique(numbers.begin(), numbers.end()), numbers.end());//去重，unique第一个重复数据的指针
 
-	for (int i = 0; i < n; ++i) {
-		int x = get(points[i].first), y = get(points[i].second);
-		sum[x][y]++;
+	for (int i = 0; i < n; ++i) {//依次处理n个点
+		int x = get(points[i].first), y = get(points[i].second);//得到当前点在numbers中的下标
+		sum[x][y]++;//统计草的数量，x，y为离散化后的numbers中的下标
 	}
 
 	for (int i = 1; i < numbers.size(); ++i)
 		for (int j = 1; j < numbers.size(); ++j)
-			sum[i][j] += sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+			sum[i][j] += sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];//求二维前缀和
 
-	int l = 1, r = 10000;
+	int l = 1, r = 10000;//二分正方形边长len
 	while (l < r) {
 		int mid = l + r >> 1;
 		if (check(mid)) r = mid;
