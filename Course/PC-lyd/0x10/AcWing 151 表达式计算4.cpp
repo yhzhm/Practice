901096b1e56ec00c1e572541a6543b363d1cee61
yhzhm <yhzhm@qq.com>
2019-04-08 16:10:35 +0800

    AcWing

--- Course/PC-lyd/0x10/AcWing 151 表达式计算4.cpp
+++ Course/PC-lyd/0x10/AcWing 151 表达式计算4.cpp
@@ -1,31 +1,31 @@
 // Created by Hz Yang on 2019.04
 #include <bits/stdc++.h>
 using namespace std;
-string s, sum;
-stack<char> opt;
-stack<int> num;
-vector<string> vstr;
+string s, sum;//s:读入的一行字符串，sum：保存转换时的数字
+stack<char> opt;//中转后时的操作符栈
+stack<int> num;//计算后缀表达式时的数字栈
+vector<string> vstr;//保存转换后的后缀表达式
 int n;
-int check(char c)
+int check(char c)//判断是何种操作符
 {
 	if (isdigit(c)) return 9;
 	if (c == '(') return 1;
 	if (c == ')') return 2;
-	if (c == '+') return 4;
 	if (c == '-') return 3;
+	if (c == '+') return 4;
 	if (c == '*') return 5;
 	if (c == '/') return 6;
 	return 7;
 }
 
-int check_priority(char c)
+int check_priority(char c)//计算符优先级
 {
 	if (c == '(') return 1;
-	else if (c == '+' || c == '-') return 2;
-	else return 3;
+	if (c == '+' || c == '-') return 2;
+	return 3;
 }
 
-int qmi(int x, int y)
+int qmi(int x, int y)//快速幂
 {
 	int res = 1;
 	while (y) {
@@ -36,32 +36,32 @@ int qmi(int x, int y)
 	return res;
 }
 
-int tonum(string s)
+int tonum(string s)//将string保存的数字转换为int
 {
 	int res = 0, i = 0, flag = 1;
-	if (s[0] == '-') flag = -1, i = 1;
-	for (; i < s.size(); ++i) {
+	if (s[0] == '-') flag = -1, i = 1;//是否负数
+	for (   ; i < s.size(); ++i)
 		res = res * 10 + (s[i] - 48);
-	}
 	return res * flag;
 }
 
 int main()
 {
 	getline(cin, s);
+	//将中缀表达式转换为后缀表达式
 	for (int i = 0; i < s.size(); ++i) {
-		char c = s[i]; string sb = "";
+		char c = s[i]; string sb = "";//sb:用于将char转换为string
 		switch (check(c)) {
 		case 1:
-			opt.push(c); break;
+			opt.push(c); break;//左括号直接入栈
 		case 2:
-			while (opt.size()) {
-				if (check(opt.top()) == 1) {opt.pop(); break;}
-				else {vstr.push_back(sb + opt.top()); opt.pop();}
+			while (opt.size()) {//右括号时，弹出栈中左括号上的所有操作符
+				if (check(opt.top()) == 1) {opt.pop(); break;}//左括号直接弹出
+				else {vstr.push_back(sb + opt.top()); opt.pop();}//其它符号加入后缀表达式
 			}
 			break;
-		case 3: sum += '-', c = '+'; case 4:  case 5: case 6:
-		case 7:
+		case 3: sum += '-', c = '+'; case 4:  case 5: case 6://将减法转换为加法
+		case 7://将栈中比当前操作符优先级高的操作符弹出，加入后缀表达式
 		{	int x = check_priority(c), y = 0;
 			if (opt.size()) y = check_priority(opt.top());
 			while (opt.size() && x <= y) {
@@ -69,17 +69,17 @@ int main()
 				if (opt.size()) y = check_priority(opt.top());
 				else y = 0;
 			}
-			opt.push(c);
+			opt.push(c);//当前操作符入栈
 			break;
 		}
 		case 9:
-			sum += c;
+			sum += c;//累加数字
 			if (!isdigit(s[i + 1]))vstr.push_back(sum), sum = "";
 			break;
 		default: break;
 		}
 	}
-	while (opt.size()) {
+	while (opt.size()) {//弹出栈中剩余操作符，加入后缀表达式
 		string sb = "";
 		vstr.push_back(sb + opt.top());
 		opt.pop();
@@ -89,17 +89,19 @@ int main()
 	// cout << endl;
 	// while (num.size()) cout << num.top() << ' ', num.pop();
 	// cout << endl;
+
+	//计算后缀表达式
 	for (int i = 0; i < vstr.size(); ++i) {
 		char c = vstr[i][0];
-		if (isdigit(c) || c == '-') {
+		if (isdigit(c) || c == '-') {//遇到数字转换后入num栈
 			n = tonum(vstr[i]);
 			num.push(n);
-		} else if (num.size() > 1) {
+		} else if (num.size() > 1) {//遇到操作符，从num栈中取出两个数字，计算后将结果入num栈
 			int	y = num.top(); num.pop();
 			int	x = num.top(); num.pop();
 			switch (check(c)) {
 			case 1: case 2: break;
-			case 3: num.push(x + y); break;
+			case 3:
 			case 4: num.push(x + y); break;
 			case 5: num.push(x * y); break;
 			case 6: num.push(x / y); break;
@@ -109,6 +111,6 @@ int main()
 		}
 
 	}
-	cout << num.top() << endl;
+	cout << num.top() << endl;//栈中剩余数字即结果
 	return 0;
 }
