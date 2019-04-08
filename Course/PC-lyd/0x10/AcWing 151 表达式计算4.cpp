6d3bcab64ea02914afa65395e78b1fa9bf61e1e6
yhzhm <yhzhm@qq.com>
2019-04-08 13:06:33 +0800

    AcWing

--- Course/PC-lyd/0x10/AcWing 151 表达式计算4.cpp
+++ Course/PC-lyd/0x10/AcWing 151 表达式计算4.cpp
@@ -4,18 +4,18 @@ using namespace std;
 string s;
 stack<char> opt;
 stack<int> num;
-vector<char> news;
+vector<char> vopt;
 int n;
 int check(char c)
 {
 	if (isdigit(c)) return 9;
-	else if (c == '(') return 1;
-	else if (c == ')') return 2;
-	else if (c == '+') return 3;
-	else if (c == '-') return 4;
-	else if (c == '*') return 5;
-	else if (c == '/') return 6;
-	else return 7;
+	if (c == '(') return 1;
+	if (c == ')') return 2;
+	if (c == '+') return 3;
+	if (c == '-') return 4;
+	if (c == '*') return 5;
+	if (c == '/') return 6;
+	return 7;
 }
 
 int check_priority(char c)
@@ -39,59 +39,53 @@ int qmi(int x, int y)
 int main()
 {
 	getline(cin, s);
-	cout << s << endl;
 	for (int i = 0; i < s.size(); ++i) {
-		char c = s[i];
-		// cout << i << ' ' << check(c) << endl;
+		char c = s[i]; int flag = 1;
 		switch (check(c)) {
 		case 1:
 			opt.push(c); break;
 		case 2:
 			while (opt.size()) {
-				if (check(opt.top() == 2)) {opt.pop(); break;}
-				else {news.push_back(opt.top()); opt.pop();}
+				if (check(opt.top()) == 1) {opt.pop(); break;}
+				else {vopt.push_back(opt.top()); opt.pop();}
 			}
 			break;
-		case 3: case 4: case 5: case 6:
+		case 3: case 4: flag = -1, c = '+'; case 5: case 6:
 		case 7:
 		{	int x = check_priority(c), y = 0;
 			if (opt.size()) y = check_priority(opt.top());
 			while (opt.size() && x <= y) {
-				news.push_back(opt.top()); opt.pop();
+				vopt.push_back(opt.top()); opt.pop();
 				if (opt.size()) y = check_priority(opt.top());
 				else y = 0;
 			}
 			opt.push(c);
 			break;
 		}
-		case 9: news.push_back(c); break;
+		case 9:
+			n = n * 10 + (c - 48);
+			if (!isdigit(s[i + 1]))num.push(n * flag), n = 0, flag = 1;
+			break;
 		default: break;
 		}
 	}
-	// cout << "ok1";
-	while (opt.size()) news.push_back(opt.top()), opt.pop();
-	for (int i = 0; i < news.size(); ++i) cout << news[i];
+	while (opt.size()) vopt.push_back(opt.top()), opt.pop();
+	for (int i = 0; i < vopt.size(); ++i) cout << vopt[i] << ' ';
 	cout << endl;
-	for (int i = 0; i < news.size(); ++i) {
-		if (isdigit(news[i])) {
-			n = n * 10 + (news[i] - 48);
-		}
-		else {
-			if (n) {
-				num.push(n);
-				n = 0;
-			}
-			char c = news[i];
-			int x = num.top(); num.pop();
-			int y = num.top(); num.pop();
-			switch (check(c)) {
-			case 3: num.push(x + y); break;
-			case 4: num.push(x - y); break;
-			case 5: num.push(x * y); break;
-			case 6: num.push(x / y); break;
-			case 7: num.push(qmi(x, y)); break;
-			default: break;
-			}
+	while (num.size()) cout << num.top() << ' ', num.pop();
+	cout << endl;
+	for (int i = 0; i < vopt.size() && num.size() > 1; ++i) {
+		int	y = num.top(); num.pop();
+		int	x = num.top(); num.pop();
+		char c = vopt[i];
+		switch (check(c)) {
+		case 1: case 2: break;
+		case 3: num.push(x + y); break;
+		case 4: num.push(x - y); break;
+		case 5: num.push(x * y); break;
+		case 6: num.push(x / y); break;
+		case 7: num.push(qmi(x, y)); break;
+		default: break;
 		}
 	}
 	cout << num.top() << endl;
